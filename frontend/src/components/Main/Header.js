61cef89a907d3224681df11b444e695389fd2626
Robert Woolbright <robwoolbright@gmail.com>
2022-07-15 20:50:27 -0400

    working on header button logic with switch statement, not working yet

--- frontend/src/components/Main/Header.js
+++ frontend/src/components/Main/Header.js
@@ -1,11 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Switch } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const [userSelect, setUserSelect] = "";
+
+  function buttonSelect() {
+    switch (userSelect) {
+      case "characterSheet":
+        console.log("worked");
+        return (
+          <div className="header-sub-button-links">
+            <NavLink className="nav-button" to="/sheet">
+              Character Sheet
+            </NavLink>
+            <NavLink className="nav-button" to="/class">
+              Class
+            </NavLink>
+            <NavLink className="nav-button" to="/sub-class">
+              Sub Class
+            </NavLink>
+            <NavLink className="nav-button" to="/feats">
+              Feats
+            </NavLink>
+            <NavLink className="nav-button" to="/spell-list">
+              Spell List
+            </NavLink>
+            <NavLink className="nav-button" to="/spell-book">
+              Spell Book
+            </NavLink>
+          </div>
+        );
+        break;
+      default:
+        return null;
+    }
+  }
+
   return (
     <header>
       <h1>Welcome to the Header Component</h1>
+      <NavLink className="nav-button" to="/">
+        Home
+      </NavLink>
+      <button onClick={setUserSelect("characterSheet") && buttonSelect}>
+        Character Sheet
+      </button>
     </header>
   );
 };
